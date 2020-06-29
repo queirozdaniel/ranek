c0483ca1f6c29409d09bf4bfd7570ff4fadfacd4
queirozdaniel <danielqueirozak77@gmail.com>
2020-06-28 22:02:48 -0300

    Criado tela inicial

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>Ranek. Venda conosco.</footer>
+</template>
+
+<script>
+export default {
+  name: "TheFooter"
+};
+</script>
+
+<style scoped>
+footer {
+  background: #84f;
+  padding: 25px 0;
+  text-align: center;
+  font-size: 1.2rem;
+  color: #fff;
+  position: relative;
+  top: 75px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="home"></div>
+  <section>
+    <h1>Compre ou Venda</h1>
+  </section>
 </template>
 
 <script>

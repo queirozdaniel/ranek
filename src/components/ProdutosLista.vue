d8074088e9d6c9373a70037dc09ae11c02a893b9
queirozdaniel <danielqueirozak77@gmail.com>
2020-06-28 22:57:33 -0300

    Utilizando Axios para obter dados de API

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -10,6 +10,8 @@
 </template>
 
 <script>
+import { api } from "../services";
+
 export default {
   name: "ProdutosLista",
   data() {
@@ -19,11 +21,9 @@ export default {
   },
   methods: {
     getProdutos() {
-      fetch("http://localhost:3000/produto")
-        .then(response => response.json())
-        .then(response => {
-          this.produtos = response;
-        });
+      api.get("/produto").then(response => {
+        this.produtos = response.data;
+      });
     }
   },
 

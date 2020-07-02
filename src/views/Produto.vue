38205a952eeb6449cabcab158ff68775671f1779
queirozdaniel <danielqueirozak77@gmail.com>
2020-07-02 01:27:47 -0300

    Criando View de Produto

--- src/views/Produto.vue
+++ src/views/Produto.vue
@@ -1,13 +1,69 @@
 <template>
-  <p>Produto {{id}}</p>
+  <section>
+    <div v-if="produto" class="produto">
+      <ul class="fotos" v-if="produto.fotos">
+        <li v-for="(foto, index) in produto.fotos" :key="index">
+          <img :src="foto.src" :alt="foto.titulo" />
+        </li>
+      </ul>
+      <div class="info">
+        <h1>{{produto.nome}}</h1>
+        <p class="preco">{{produto.preco | numeroPreco}}</p>
+        <p class="descricao">{{produto.descricao}}</p>
+        <button class="btn" v-if="produto.vendido === 'false'">Comprar</button>
+        <button class="btn-vendido" v-else disabled>Produto vendido</button>
+      </div>
+    </div>
+    <PaginaCarregando v-else />
+  </section>
 </template>
 
 <script>
+import { api } from "../services";
+
 export default {
   name: "Produto",
-  props: ["id"]
+  props: ["id"],
+  data() {
+    return {
+      produto: null
+    };
+  },
+  methods: {
+    getProdutos() {
+      api.get(`/produto/${this.id}`).then(r => {
+        this.produto = r.data;
+      });
+    }
+  },
+  created() {
+    this.getProdutos();
+  }
 };
 </script>
 
-<style>
+<style scoped>
+.produto {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 30px;
+  max-width: 900px;
+  padding: 60px 20px;
+  margin: 0 auto;
+}
+
+.preco {
+  color: #e80;
+  font-weight: bold;
+  font-size: 1.5rem;
+  margin-bottom: 40px;
+}
+
+.descricao {
+  font-size: 1.2rem;
+}
+
+.btn {
+  margin-top: 60px;
+}
 </style>

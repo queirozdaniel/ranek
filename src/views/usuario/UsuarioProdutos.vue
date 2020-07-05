ec22bc09f45e546c914c8cccc87167bd2aa97764
queirozdaniel <danielqueirozak77@gmail.com>
2020-07-04 23:36:24 -0300

    Adicionando produtos

--- src/views/usuario/UsuarioProdutos.vue
+++ src/views/usuario/UsuarioProdutos.vue
@@ -1,10 +1,62 @@
 <template>
-  <p>Produtos</p>
+  <section>
+    <h2>Adicionar Produtos</h2>
+    <ProdutosAdicionar />
+    <h2>Seus Produtos</h2>
+
+    <transition-group v-if="usuario_produtos" name="list" tag="ul">
+      <li v-for="(produto, index) in usuario_produtos" :key="index">
+        <ProdutoItem :produto="produto">
+          <p>{{produto.descricao}}</p>
+        </ProdutoItem>
+      </li>
+    </transition-group>
+  </section>
 </template>
 
 <script>
-export default {};
+import ProdutosAdicionar from "../../components/ProdutosAdicionar";
+import ProdutoItem from "../../components/ProdutoItem";
+import { mapState, mapActions } from "vuex";
+
+export default {
+  name: "UsuarioProdutos",
+  components: {
+    ProdutosAdicionar,
+    ProdutoItem
+  },
+  computed: {
+    ...mapState(["login", "usuario", "usuario_produtos"])
+  },
+  methods: {
+    ...mapActions(["getUsuarioProdutos"])
+  },
+  watch: {
+    login() {
+      this.getUsuarioProdutos();
+    }
+  },
+  created() {
+    if (this.login) {
+      this.getUsuarioProdutos();
+    }
+  }
+};
 </script>
 
-<style>
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+}
+
+.list-enter,
+.list-leave-to {
+  opacity: 0;
+  transform: translate3d(20px, 0, 0);
+}
+
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.3s;
+}
 </style>
